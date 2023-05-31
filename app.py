a5aa47f0a82a157fcf6094f54af52beed9d2a773
Alef Cordeiro <alefsp96@hotmail.com>
2023-05-31 19:15:22 -0300

    Adicionado sessao de respostas

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def create_dataframe_section(df):
 
 def create_answers_section(df):
     st.title("Main Questions Answers")
-
     st.header("First Round")
     st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
 
@@ -65,11 +64,13 @@ def create_answers_section(df):
 
 def main():    
 
-    df_raw = load_data()
+    df = load_data()
 
-    create_dataframe_section(df_raw)
+    create_dataframe_section(df)
 
-    st.dataframe(df_raw)
+    create_answers_section(df)
+
+    st.dataframe(df)
 
 if __name__ == '__main__':
     main()
